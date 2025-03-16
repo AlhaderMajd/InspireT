359bd3fa4981784b10bc722aead80dc175182392
AlhaderMajd <mjd0216623@ju.edu.jo>
2025-03-16 13:54:44 +0300

    i am in video 17 with chad

--- dev-spring-boot/demo/src/main/java/com/example/demo/rest/FunRestController.java
+++ dev-spring-boot/demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -3,13 +3,12 @@ package com.example.demo.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //Set up rest controller
 public class FunRestController {
     //expose "/" that return "Hello World"
-    @GetMapping("/")
+    @GetMapping("/") //Handle HTTP GET requests
     public String sayHello(){
-
-        return "Hello World";
+        return "Majd Abdullah Alhader";
     }
 
 }
