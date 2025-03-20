a18ca7353f52163faca1e7db93380f8cab098d8f
AlhaderMajd <mjd0216623@ju.edu.jo>
2025-03-20 11:45:41 +0300

    Finish section 1 in spring course

--- dev-spring-boot/demo/src/main/java/com/example/demo/rest/FunRestController.java
+++ dev-spring-boot/demo/src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,14 +1,29 @@
 package com.example.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //Set up rest controller
 public class FunRestController {
+
+    //inject properties for: coach.name and taem.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: "+coachName+", Team name: "+teamName;
+    }
+
     //expose "/" that return "Hello World"
     @GetMapping("/") //Handle HTTP GET requests
     public String sayHello(){
-        return "Hellow!";
+        return "Hello World";
     }
 
     @GetMapping("/workout")
