d4e87e4758494d45337313ffee73af31830e8ece
AlhaderMajd <mjd0216623@ju.edu.jo>
2025-03-23 12:10:07 +0300

    i'm in video 58

--- dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -11,30 +11,16 @@ public class DemoController {
 
     //define a private field for the dependency
     private Coach myCoach;
-    private Coach anotherCoach;
 
     //define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
-                          @Qualifier("cricketCoach") Coach theAnotherCoach)
-    {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("Class info: " + getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
-
-    /*@Autowired
-    public void setter (Coach theCoach){
-        myCoach = theCoach;
-    }*/
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
-    }
-
-    @GetMapping("/check")
-    public String check(){
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach==anotherCoach);
     }
 }
