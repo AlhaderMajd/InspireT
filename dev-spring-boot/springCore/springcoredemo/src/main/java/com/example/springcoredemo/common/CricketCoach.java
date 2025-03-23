80f17a621df66774423fe72aba1d12900762240c
AlhaderMajd <mjd0216623@ju.edu.jo>
2025-03-23 12:25:37 +0300

    Section 2 Done

--- dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/common/CricketCoach.java
+++ dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/common/CricketCoach.java
@@ -16,7 +16,7 @@ public class CricketCoach implements Coach {
     }
 
     //define our init method
-    @PostConstruct
+    /*@PostConstruct
     public void doMyStartupStuff(){
         System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
     }
@@ -24,7 +24,7 @@ public class CricketCoach implements Coach {
     @PreDestroy
     public void doMyCleanupStuff(){
         System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
-    }
+    }*/
 
     @Override
     public String getDailyWorkout(){
--- dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ dev-spring-boot/springCore/springcoredemo/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -14,7 +14,7 @@ public class DemoController {
 
     //define a constructor for dependency injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(@Qualifier("swimCoach") Coach theCoach) {
         System.out.println("Class info: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
