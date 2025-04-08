2c8b30ad460f76cb0e78bee72cba5df206517d09
AlhaderMajd <mjd0216623@ju.edu.jo>
2025-04-08 10:59:49 +0300

    293 done

---
+++ dev-spring-boot/09-section-springboot/01-jpa-one-to-one-uni/src/main/java/com/example/cruddemo/CruddemoApplication.java
@@ -0,0 +1,58 @@
+package com.example.cruddemo;
+
+import com.example.cruddemo.dao.AppDAO;
+import com.example.cruddemo.entity.Instructor;
+import com.example.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		return runner -> {
+			//creatInstructor(appDAO);
+
+			//findInstructor(appDAO);
+			
+			deleteInstructor(appDAO);
+		};
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Deleting instructor id: "+theId);
+		appDAO.deleteInstructorById(theId);
+		System.out.println("Done");
+	}
+
+	private void findInstructor(AppDAO appDAO) {
+		int theId = 1;
+		System.out.println("Finding instructor id: "+theId);
+		Instructor tempInstructor = appDAO.findInstructorById(theId);
+		System.out.println("tempInstructor: "+tempInstructor);
+		System.out.println("the associate instructorDetail only: "+tempInstructor.getInstructorDetail());
+	}
+
+	private void creatInstructor(AppDAO appDAO) {
+
+		Instructor tempInstructor =
+				new Instructor("majd", "alhader","m@gmail.com");
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http", "FB");
+
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		System.out.println("Saving instructor: "+tempInstructor);
+		appDAO.save(tempInstructor);
+
+		System.out.println("Done");
+	}
+}
